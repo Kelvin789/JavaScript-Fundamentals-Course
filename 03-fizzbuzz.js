7698957a342e697f646e6a9de0826441f78686a0
Kelvin789 <67801802+Kelvin789@users.noreply.github.com>
2025-08-22 23:45:29 +0100

    Create 03-fizzbuzz.js

---
+++ 03-fizzbuzz.js
@@ -0,0 +1,16 @@
+const output = fizzBuzz(true);
+console.log(output);
+
+function fizzBuzz(input) {
+    if (typeof input !== 'number')
+        return NaN;
+  
+    if ((input % 5 === 0) && (input % 3 === 0))
+        return 'FizzBuzz';
+    else if (input % 5 === 0)
+        return 'Buzz';
+    else if (input % 3 === 0)
+        return 'Fizz';
+    else    
+        return input;
+}
