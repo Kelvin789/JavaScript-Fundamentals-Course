e392b342d2d31ca6b8591f79c778c3b137921091
Kelvin789 <67801802+Kelvin789@users.noreply.github.com>
2025-08-22 23:38:42 +0100

    Create 01-max-of-two-numbers.js

---
+++ 01-max-of-two-numbers.js
@@ -0,0 +1,9 @@
+let number = MaximumNumber(9, 1);
+console.log(number);
+
+function maximumNumber(number1, number2) {
+    if (number1 > number2)
+        return number1;
+    else
+        return number2;
+}
